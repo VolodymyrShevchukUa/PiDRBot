f5e47826ea9ae006517a411ca234172ff3b2ba29
AdskiyDrochila1488 <volodymyr.shevchuk.ua@gmail.com>
2022-05-08 22:44:24 +0300

    тепер bot token & bot name викликаються через properties

--- src/main/java/PDRBot2.java
+++ src/main/java/PDRBot2.java
@@ -1,22 +1,28 @@
 import adapter.sender.SenderTelegrambots;
 import handlers.MainMenuStrategy;
 import handlers.Strategy;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 
 public class PDRBot2 extends SenderTelegrambots {
-    private static final String TelegramBotName = "PDRbot";
-    private static final String TelegramBotToken = "5184808348:AAGqn7MBsuOsdTCGtnA1GrN6VwmavE0m8LY";
+    private static final String PATH = "src/main/resources/bot.properties";
+    private final Properties propertie = getProperty();
+    private final String TelegramBotName = propertie.getProperty("bot.name");
+    private final String TelegramBotToken = propertie.getProperty("bot.token");
     private Strategy currentStrategy = new MainMenuStrategy(this);
+
+    // Також можна замутити це гавно через конструктор
     public static void main(String[] args) {
 
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new PDRBot2());
+            telegramBotsApi.registerBot(new PDRBot3());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
@@ -42,7 +48,18 @@ public class PDRBot2 extends SenderTelegrambots {
         return TelegramBotToken;
     }
 
+    private Properties getProperty() {
 
+        FileInputStream fis;
+        Properties properties = new Properties();
+        try {
+            fis = new FileInputStream(PATH);
+            properties.load(fis);
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+        return properties;
+    }
 }
 
 
