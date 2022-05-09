0a175bb1e5171db97258cbb6493a52039cf0d1a7
narnanam <narnanam@gmail.com>
2022-05-09 13:20:24 +0300

    refactor Question class

--- src/main/java/entity/Question.java
+++ src/main/java/entity/Question.java
@@ -1,13 +1,12 @@
 package entity;
 
-import adapter.message.PhotoMessage;
 import adapter.message.MessageI;
+import adapter.message.PhotoMessage;
 import adapter.message.TextMessage;
 import org.json.JSONObject;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import utils.json.QuestionToJSON;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,16 +15,21 @@ import java.util.List;
 // Екземпляр класу questions матиме два параметри,
 // Їх ми пробуєм записати в масив [] об'єктів questions {} в JSON
 public class Question {
+    public static final String NULL_IMAGE = "null image";
+    public static final String CAPTION = "Caption";
+    public static final String CORRECT_BUTTON = "CorrectButton";
+    public static final String URL = "Url";
+    public static final String COUNT_OF_BUTTON = "CountOfButton";
     private final String caption;
     private final String url;
-    private final Integer correctButton;
+    private final int correctButton;
     private final int countOfButton;
 
     public Question(JSONObject jsonObject) {
-        url = jsonObject.getString(QuestionToJSON.URL);
-        correctButton = jsonObject.getInt(QuestionToJSON.CORRECT_BUTTON);
-        countOfButton = jsonObject.getInt(QuestionToJSON.COUNT_OF_BUTTON);
-        caption = jsonObject.getString(QuestionToJSON.CAPTION);
+        url = jsonObject.getString(URL);
+        correctButton = jsonObject.getInt(CORRECT_BUTTON);
+        countOfButton = jsonObject.getInt(COUNT_OF_BUTTON);
+        caption = jsonObject.getString(CAPTION);
     }
 
     public Question(String image, int correctButton, String caption, int button) {
@@ -35,43 +39,27 @@ public class Question {
         this.countOfButton = button;
     }
 
-    public Integer getCorrectButon() {
-        return correctButton;
-    }
-    
-    public String getUrl() {
-        return url;
-    }
-
-    public String getCaption() {
-        return caption;
-    }
-
-    public int getCountOfButton() {
-        return countOfButton;
-    }
-
-    public boolean hasPhoto(){
-        return !url.equals(QuestionToJSON.NULL_IMAGE);
+    private boolean hasPhoto() {
+        return !url.equals(NULL_IMAGE);
     }
 
     public MessageI createMessage(long chatId) {
-        InlineKeyboardMarkup inlineKeyboardMarkup = createInlineKeyboardButtonList(getCountOfButton(), getCorrectButon());
+        InlineKeyboardMarkup inlineKeyboardMarkup = createInlineKeyboardButtonList();
         MessageI messageI;
         if (hasPhoto()) {
-            messageI = new PhotoMessage(chatId, getCaption(), new InputFile(getUrl()), inlineKeyboardMarkup);
+            messageI = new PhotoMessage(chatId, caption, new InputFile(url), inlineKeyboardMarkup);
         } else {
-            messageI = new TextMessage(chatId, getCaption(), inlineKeyboardMarkup);
+            messageI = new TextMessage(chatId, caption, inlineKeyboardMarkup);
         }
         return messageI;
     }
 
-    private InlineKeyboardMarkup createInlineKeyboardButtonList(int countOfButton, int trueButton) {
+    private InlineKeyboardMarkup createInlineKeyboardButtonList() {
         List<InlineKeyboardButton> buttons = new ArrayList<>();
         InlineKeyboardMarkup in = new InlineKeyboardMarkup();
         for (int i = 1; i < countOfButton; i++) {
             buttons.add(InlineKeyboardButton.builder()
-                    .callbackData(i == trueButton ? "true" : "false")
+                    .callbackData(i == correctButton ? "true" : "false")
                     .text(i + "✅")
                     .build());
         }
@@ -79,5 +67,12 @@ public class Question {
         return in;
     }
 
-
+    public JSONObject toJSONObject() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(CAPTION, caption);
+        jsonObject.put(CORRECT_BUTTON, correctButton);
+        jsonObject.put(URL, url);
+        jsonObject.put(COUNT_OF_BUTTON, correctButton);
+        return jsonObject;
+    }
 }
--- src/main/java/utils/json/QuestionToJSON.java
+++ src/main/java/utils/json/QuestionToJSON.java
@@ -10,42 +10,22 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class QuestionToJSON {
-    public static final String NULL_IMAGE = "null image";
-    public static final String CAPTION = "Caption";
-    public static final String CORRECT_BUTTON = "CorrectButton";
-    public static final String URL = "Url";
-    public static final String COUNT_OF_BUTTON = "CountOfButton";
     public static final String QUESTIONS = "Questions";
-    public static final String PATH = "src/main/resources/Questions2.json";
+    public static final String PATH = "src/main/resources/Questions.json";
 
     public static void main(String[] args) {
-
-       List<Question> questions = new Pars("https://vodiy.ua/pdr/test/?complect=6&bilet=",100).pars();
-       JSONArray array = new JSONArray();
-       for(Question q: questions){
-           JSONObject jsonObject = new JSONObject();
-           jsonObject.put(CAPTION,q.getCaption());
-           jsonObject.put(CORRECT_BUTTON,q.getCorrectButon());
-           jsonObject.put(URL,q.getUrl());
-           jsonObject.put(COUNT_OF_BUTTON,q.getCountOfButton());
-           array.put(jsonObject);
-       }
-       JSONObject result = new JSONObject();
-       result.put(QUESTIONS,array);
-       System.out.println(result.toString());
-
-        String content = result.toString();
-
+        List<Question> questions = new Pars("https://vodiy.ua/pdr/test/?complect=6&bilet=", 100).pars();
+        JSONArray array = new JSONArray();
+        for (Question q : questions) {
+            array.put(q.toJSONObject());
+        }
+        JSONObject result = new JSONObject();
+        result.put(QUESTIONS, array);
         try {
-            Files.writeString(Paths.get(PATH), content);
+            Files.writeString(Paths.get(PATH), result.toString());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
     }
-
-
 }
